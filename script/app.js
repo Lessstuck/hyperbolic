a72d2cf33b380dd20b80859c7b751466b8b5593a
Lessstuck <38271153+Lessstuck@users.noreply.github.com>
2022-12-18 14:35:33 -0700

    Delete rest of testMode garbage

--- script/app.js
+++ script/app.js
@@ -2,17 +2,6 @@ import { Instrument } from "./click.js";
 import { dragPositions } from "./mousepick.js";
 import { startImpulse, stopReset } from "./mousepick.js";
 
-let testMode = "run";
-$("#run").click(function () {
-  testMode = "run";
-});
-$("#low").click(function () {
-  testMode = "low";
-});
-$("#high").click(function () {
-  testMode = "high";
-});
-
 /// maps dragPositions (-5 to +5) to morph values (0 to 100)
 /// i = body index, j = axis index
 let dragPositionClipped;
